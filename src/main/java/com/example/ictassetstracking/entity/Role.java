12c40014ff4bff6cf74c064846bbc89d3ca73d9c
faraja <farajakomba2000@gmail.com>
2026-06-24 16:51:11 +0300

    updating branch

--- src/main/java/com/example/ictassetstracking/entity/Role.java
+++
@@ -1,7 +0,0 @@
-package com.example.ictassetstracking.entity;
-
-public enum Role {
-    ROLE_USER,
-    ROLE_MANAGER,
-    ROLE_ADMIN
-}
--- src/main/java/com/example/ictassetstracking/service/ReleasedAssetService.java
+++
@@ -1,9 +0,0 @@
-package com.example.ictassetstracking.service;
-
-import java.util.List;
-
-import com.example.ictassetstracking.dto.ReleasedAssetDto;
-
-public interface ReleasedAssetService {
-    List<ReleasedAssetDto> listAllReleasedAssets();
-}
